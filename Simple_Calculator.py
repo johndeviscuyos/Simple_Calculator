ae8b8867864354c8714445784b5bce193610b42c
Devis <johndeviscuyos@gmail.com>
2024-05-06 08:40:22 +0800

    Added Pseudocode

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,8 +1,8 @@
 #Ask the user if the operation is Addition, Subtraction, Multiplication and Division.
-#Ask the user to type the letter of the corresponding operation eg. A for Addition, S for Subtraction, M for Multiplication and D for Division
-#Show an exception if user inputs an integer
-#Ask the user to enter the first number
-#Ask the user to enter the second number
-#Show an exception if user inputs a string
-#Show the result
-#Ask the user if he/she wants to try it again
+#Ask the user to type the letter of the corresponding operation eg. A for Addition, S for Subtraction, M for Multiplication and D for Division.
+#Show an exception if user inputs an integer.
+#Ask the user to enter the first number.
+#Ask the user to enter the second number.
+#Show an exception if user inputs a string.
+#Show the result.
+#Ask the user if he/she wants to try it again.
