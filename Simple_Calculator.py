3725a123fd967ee646a7bba43de4e840370cad97
Devis <johndeviscuyos@gmail.com>
2024-05-06 08:44:30 +0800

    Added introduction and added user to input what type of operation they want to do

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,5 +1,11 @@
 #Ask the user if the operation is Addition, Subtraction, Multiplication and Division.
+print("Welcome to the Simple Calculator")
+print("This calculator will help you in simple Addition, Subtraction, Multiplication or Division")
 #Ask the user to type the letter of the corresponding operation eg. A for Addition, S for Subtraction, M for Multiplication and D for Division.
+try:
+    operation = str(input("Type A for Addition, S for Subtraction, M for Multiplication or D for Division. "))
+
+
 #Show an exception if user inputs an integer.
 #Ask the user to enter the first number.
 #Ask the user to enter the second number.
